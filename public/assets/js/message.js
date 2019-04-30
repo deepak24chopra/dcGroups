5ac70c3afe35699cc35207bbf2a9f6f91c6249a0
deepak chopra <deepak24chopra@gmail.com>
2019-04-30 07:32:58 +0530

    message

--- public/assets/js/message.js
+++ public/assets/js/message.js
@@ -18,7 +18,32 @@ function getMessages(groupId) {
         })
         .catch(function(error) {
             console.log(error);
+        });
+}
+
+function showMessages() {}
+
+function createMessage() {
+    fetch("/api/message/create", {
+            method: "post",
+            headers: {
+                "content-type": "application/json; charset=UTF-8"
+            },
+            body: `{"groupId": ${group._id}, "userId": ${user._id}}`
+        }).then(function(data) {
+            if (data.status == 404) {
+                console.log("API not found");
+                return;
+            }
+            return data;
+        }).then(function(data) {
+            return data.json();
+        }).then(function(data) {
+            messages.push(data);
         })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
 
 let messages = [];
